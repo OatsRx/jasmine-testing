44d0645217f54010d507d43a98914cb665151cc3
EC2 Default User <ec2-user@ip-172-31-86-111.ec2.internal>
2019-12-05 15:52:46 +0000

    added additional error test to calcSpec.js

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -5,7 +5,10 @@ describe("Calculator", function(){
         });
         it("should return 26", function() {
             expect(addition(7,19)).toBe(26);
-        })
+        });
+        it("shoud return an error if we dont supply two numbers", function() {
+            expect(addition("Hitchhikers", "Guide")).toBe("Error!");
+        });
     });
 });
 
