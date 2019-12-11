03a4b7769608e36b07942e41103d41c626711dd7
EC2 Default User <ec2-user@ip-172-31-86-111.ec2.internal>
2019-12-11 12:24:12 +0000

    added calculator object and add function prototype

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,12 +1,10 @@
 //Calculator object
-
 Calculator = function() {
-    this.value = 0;
+    this.value = 0; //calculator starting value will be 0
 }
 
 
-//prototypes of Calculator object
-
+//prototypes of Calculator object addition function
 Calculator.prototype.add = function(number){
     if (typeof(number) == "number") {
         this.value += number;
