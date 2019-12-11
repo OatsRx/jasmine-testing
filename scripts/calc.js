9d1c06f04090f4b76e7f03cec15a1451834419fd
EC2 Default User <ec2-user@ip-172-31-86-111.ec2.internal>
2019-12-11 12:00:16 +0000

    added comments for understanding

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,6 +1,11 @@
+//Calculator object
+
 Calculator = function() {
     this.value = 0;
 }
+
+
+//prototypes of Calculator object
 
 Calculator.prototype.add = function(number){
     if (typeof(number) == "number") {
