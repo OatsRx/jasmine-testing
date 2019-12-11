3da3f27f6b8672d6f0e5f7b78950d8b779c7ca5d
EC2 Default User <ec2-user@ip-172-31-86-111.ec2.internal>
2019-12-11 12:34:18 +0000

    added beforeEach function to reset value of Calculator before each test instance

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,6 +1,8 @@
 describe("Calculator", function(){
     
+    beforeEach(function() { //before each test instance we can reset the value of Calculator back to 0
     var calc = new Calculator; //assigning "Calculator" object to variable "calc"
+    });
     
     describe("Addition tests", function() {
         it("should return 42", function() {
@@ -9,13 +11,13 @@ describe("Calculator", function(){
             expect(calc.value).toBe(42);
         });
         it("should return 26", function() {
-            calc.add(7)
+            calc.add(7)//"calc.add" functon taken from prototype of "Calculator" object
             calc.add(19)
             expect(calc.value).toBe(26);
         });
         it("shoud return an error if we dont supply two numbers", function() {
             spyOn(window, "alert");
-            calc.add("Hitchhikers");
+            calc.add("Hitchhikers");//"calc.add" functon taken from prototype of "Calculator" object will return error as data type is string not number
             expect(window.alert).toHaveBeenCalledWith
                 ("Error!");
         });
